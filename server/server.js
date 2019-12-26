ee6b3eb83a6fc21917f056080402b8b29755b240
sakshi <sanjaykumar@sanjays-MacBook-Air.local>
2019-12-27 00:20:14 +0530

    add post todos

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/users');
-
+var {ObjectId} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -22,6 +22,30 @@ app.post('/todos', (req,res) => {
   });
 });
 
+app.get('/todos', (req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(err)=>{
+    res.send(e);
+  })
+});
+
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) =>{
+    if(!todo){
+        return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+});
 
 app.listen(3000, ()=>{
   console.log('started on port 3000');
