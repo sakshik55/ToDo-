92dd5e67fe097286e6c5a3df152a293380a4c0c4
sakshi <sanjaykumar@sanjays-MacBook-Air.local>
2019-12-26 18:58:13 +0530

    add post todos

---
+++ server/models/users.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+
+var User = mongoose.model('User',{
+  email:{
+    type: String,
+    require: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {User};
